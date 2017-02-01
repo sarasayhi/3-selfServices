fa299baa59f2b1414a675403b4e198936a13be03
sarasayhi <sarasayhi@163.com>
2017-02-02 01:31:16 +0800

    [ADD]新增模板文件

---
+++ js/default.js
@@ -0,0 +1,11 @@
+/* Copyright (C) 2017
+ * This file is part of my graduation project.
+ *
+ * filename : default.js
+ * function :
+ * action   :
+ * version  : 1.0
+ * author   : MarissaMan
+ * date     : 2017/2/1
+ * modify   :
+ */
